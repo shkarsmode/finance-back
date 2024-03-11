6df3f238cf952d434b724f3018025c5c228f7fad
Daniil Shkarupa <d.shkarupa@boosteroid.com>
2024-03-11 13:14:30 +0200

    Add user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,36 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
+import { UserModule } from './entities/user/user.module';
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: `environments/.env`,
+        }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: (configService: ConfigService) => ({
+                type: 'postgres',
+                host: configService.get<string>('POSTGRES_HOST'),
+                port: Number(configService.get<number>('POSTGRES_PORT')),
+                password: configService.get<string>('POSTGRES_PASSWORD'),
+                username: configService.get<string>('POSTGRES_USER'),
+                database: configService.get<string>('POSTGRES_DATABASE'),
+                synchronize: true,
+                entities: [__dirname + '***.entity{.js, .ts}'],
+                extra: {
+                    ssl: {
+                        rejectUnauthorized: false,
+                    },
+                },
+            }),
+            inject: [ConfigService],
+        }),
+        UserModule,
+    ],
     controllers: [AppController],
     providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+    app.setGlobalPrefix('api');
+    app.enableCors();
+    
     await app.listen(3000);
 }
 bootstrap();
