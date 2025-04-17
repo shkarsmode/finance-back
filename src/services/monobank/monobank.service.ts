f5e1b997d7030d55679c71bc4fa2fbf25e5ec297
Daniil Shkarupa <d.shkarupa@boosteroid.com>
2025-04-17 15:51:16 +0300

    Add timeout to pipe #2

--- src/services/monobank/monobank.service.ts
+++ src/services/monobank/monobank.service.ts
@@ -73,7 +73,7 @@ export class MonobankService {
                     timeout: 10000,
                 })
                 .pipe(
-                    timeout(9000),
+                    timeout(5000),
                     tap(() => (this.lastRequestTransactionsTime = Date.now())),
                     catchError((error: AxiosError) => {
                         console.warn(
